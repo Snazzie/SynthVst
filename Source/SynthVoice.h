7f7812c4cf1faa58c9550d357a58f212519f5f61
Aaron <acoop133@hotmail.com>
2019-05-05 04:29:34 +0100

    Working with sound

--- Source/SynthVoice.h
+++ Source/SynthVoice.h
@@ -11,7 +11,7 @@
 #pragma once
 #include "../JuceLibraryCode/JuceHeader.h"
 #include "SynthSound.h"
-
+#include "maximilian.h"
 class SynthVoice : public SynthesiserVoice
 {
 public:
@@ -22,18 +22,47 @@ public:
 
 	void startNote(int midiNoteNumber, float velocity, SynthesiserSound* sound, int currentPitchWheelPosition) override
 	{
+		env1.trigger = 1;
 		frequency = MidiMessage::getMidiNoteInHertz(midiNoteNumber);
-		std::cout << midiNoteNumber << std::endl;
+		level = velocity;
 	}
 	void stopNote(float velocity, bool allowTailOff) override
 	{
-		clearCurrentNote();
+		env1.trigger = 0;
+		allowTailOff = true;
+
+
+		if (velocity == 0)
+			clearCurrentNote();
 	}
-	void pitchWheelMoved(int newPitchWheelValue) override{}
-	void controllerMoved(int controllerNumber, int newControllerValue) override{}
-	void renderNextBlock(AudioBuffer<float>& outputBuffer, int startSample, int numSamples) override{}
+	void pitchWheelMoved(int newPitchWheelValue) override {}
+	void controllerMoved(int controllerNumber, int newControllerValue) override {}
+	void renderNextBlock(AudioBuffer<float>& outputBuffer, int startSample, int numSamples) override
+	{
+		
+		env1.setAttack(.2);
+		env1.setDecay(500);
+		env1.setSustain(0.8);
+		env1.setRelease(2000.0);
+
+
+		for (int sample = 0; sample < numSamples; ++sample)
+		{
+			double theWave = osc1.sinewave(frequency);
+			double theSound = env1.adsr(theWave, env1.trigger) * level;
+
+			for (int channel = 0; channel < outputBuffer.getNumChannels(); ++channel)
+			{
+				outputBuffer.addSample(channel, startSample, theSound);
+			}
+			++startSample;
+		}
+	}
 
 private:
 	double level;
 	double frequency;
+
+	maxiOsc osc1;
+	maxiEnv env1;
 };
